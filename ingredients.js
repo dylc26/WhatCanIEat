2adcaeec78948d509fbbdb9a634e7115e46e247c
Dylan Curtis <107953939+dylc26@users.noreply.github.com>
2022-07-21 17:54:40 -0230

    Update ingredients.js
    variable name cleanup

--- ingredients.js
+++ ingredients.js
@@ -8,57 +8,57 @@ window.addEventListener('load', () => {
     form.addEventListener('submit', e => {
         e.preventDefault();
 
-        const exi = input.value;
+        const excludeIngredient = input.value;
 
-        const exi_el = document.createElement("div");
-        exi_el.classList.add("exi");
+        const excludedIngredient_el = document.createElement("div");
+        excludedIngredient_el.classList.add("excludeIngredient");
 
-        const exi_content_el = document.createElement("div");
-        exi_content_el.classList.add("content");
+        const excludedIngredient_content_el = document.createElement("div");
+        excludedIngredient_content_el.classList.add("content");
 
-        exi_el.appendChild(exi_content_el);
+        excludedIngredient_el.appendChild(excludedIngredient_content_el);
 
-        const exi_input_el = document.createElement("input");
-        exi_input_el.classList.add("text");
-        exi_input_el.type = "text";
-        exi_input_el.value = exi;
-        exi_input_el.setAttribute("readonly", "readonly");
+        const excludedIngredient_input_el = document.createElement("input");
+        excludedIngredient_input_el.classList.add("text");
+        excludedIngredient_input_el.type = "text";
+        excludedIngredient_input_el.value = excludeIngredient;
+        excludedIngredient_input_el.setAttribute("readonly", "readonly");
         
-        exi_content_el.appendChild(exi_input_el);
+        excludedIngredient_content_el.appendChild(excludedIngredient_input_el);
     
-        const exi_actions_el = document.createElement("div");
-        exi_actions_el.classList.add("actions");
+        const excludedIngredient_actions_el = document.createElement("div");
+        excludedIngredient_actions_el.classList.add("actions");
 
-		const exi_edit_el = document.createElement("button");
-		exi_edit_el.classList.add("edit");
-		exi_edit_el.innerHTML = "Edit";
+		const excludedIngredient_edit_el = document.createElement("button");
+		excludedIngredient_edit_el.classList.add("edit");
+		excludedIngredient_edit_el.innerHTML = "Edit";
 
-        const exi_remove_el = document.createElement("button");
-        exi_remove_el.classList.add("remove");
-        exi_remove_el.innerHTML = "Remove";
+        const excludedIngredient_remove_el = document.createElement("button");
+        excludedIngredient_remove_el.classList.add("remove");
+        excludedIngredient_remove_el.innerHTML = "Remove";
 
-        exi_actions_el.appendChild(exi_edit_el);
-		exi_actions_el.appendChild(exi_remove_el);
+        excludedIngredient_actions_el.appendChild(excludedIngredient_edit_el);
+		excludedIngredient_actions_el.appendChild(excludedIngredient_remove_el);
 
-        exi_el.appendChild(exi_actions_el);
+        excludedIngredient_el.appendChild(excludedIngredient_actions_el);
 
-        list_el.appendChild(exi_el);
+        list_el.appendChild(excludedIngredient_el);
 
         input.value = "";
 
-		exi_edit_el.addEventListener('click', () => {
-			if (exi_edit_el.innerText.toLowerCase() === "edit") {
-				exi_input_el.removeAttribute("readonly");
-				exi_input_el.focus();
-				exi_edit_el.innerText = "Save";
+		excludedIngredient_edit_el.addEventListener('click', () => {
+			if (excludedIngredient_edit_el.innerText.toLowerCase() === "edit") {
+				excludedIngredient_input_el.removeAttribute("readonly");
+				excludedIngredient_input_el.focus();
+				excludedIngredient_edit_el.innerText = "Save";
 			} else {
-				exi_input_el.setAttribute("readonly", "readonly");
-				exi_edit_el.innerText = "Edit";	
+				excludedIngredient_input_el.setAttribute("readonly", "readonly");
+				excludedIngredient_edit_el.innerText = "Edit";	
 			}
 		});	
 
-        exi_remove_el.addEventListener('click', () => {
-            list_el.removeChild(exi_el);
+        excludedIngredient_remove_el.addEventListener('click', () => {
+            list_el.removeChild(excludedIngredient_el);
         });
     });
 });
